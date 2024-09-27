1a72927856f36f8171650ac2003c0a5ed8c02395
Chikyu64 <bonnietron624@gmail.com>
2024-09-26 20:56:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+/* defining variables
+*/
+let num = 10
+let numOne = num+0;
+let numTwo = num*4;
+let numThree = numTwo-1; 
+
+const str = `${message} ${numOne}, ${numTwo}, ${numThree}`
+console.log(str)
+
+alert(str)
